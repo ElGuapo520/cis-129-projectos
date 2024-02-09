31d38a80d283b0d84bd4492e2e84989828178904
ElGuapo520 <130625328+ElGuapo520@users.noreply.github.com>
2024-02-08 23:14:22 -0700

    Update cis129_lab02_shortStory.py
    removed print hello world and replaced with short story of five sentences.

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,11 @@
-print('Hello World!')
+# Author: Alex Garcia Suarez
+# Cis 129 spring 2024
+# Lab 02
+# Prints a simple short story
+
+
+
+print ('There Once was a man from Kentuckeh.')
+print ('He liked to eat out of Buckets.')
+print ('People loved it.')
+print('The end.')
